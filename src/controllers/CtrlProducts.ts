3fb68142d7e1e9e002fd08d1df666d666a2bc7d2
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-01-12 20:27:35 +0200

    Added getProductsById

--- src/controllers/CtrlProducts.ts
+++ src/controllers/CtrlProducts.ts
@@ -1,14 +1,27 @@
 import { BetterSQLite3Database } from 'drizzle-orm-sqlite/better-sqlite3';
+import { Request } from 'express';
 
 import { Products } from '../data/repositories/Products';
 
 export class CtrlProducts {
-  async getAllProducts(req, res, next) {
+  async getAllProducts(req: Request, res, next) {
     const db: BetterSQLite3Database = req.body.connection;
     const { limit, page } = req.query;
 
     const products = new Products(db);
     const data = await products.getAllProducts(+limit, +page);
+
+    res.status(200).json({
+      status: 'success',
+      data,
+    });
+  }
+
+  async getProductsById(req: Request, res, next) {
+    const db: BetterSQLite3Database = req.body.connection;
+    const { id } = req.params;
+    const products = new Products(db);
+    const data = await products.getProductById(+id);
 
     res.status(200).json({
       status: 'success',
