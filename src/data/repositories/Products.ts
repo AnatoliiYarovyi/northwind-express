93c415f14f0ead373e88706e88b716c0ca526f8e
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-01-12 20:29:53 +0200

    added getProductById

--- src/data/repositories/Products.ts
+++ src/data/repositories/Products.ts
@@ -1,6 +1,8 @@
 import { BetterSQLite3Database } from 'drizzle-orm-sqlite/better-sqlite3';
+import { eq } from 'drizzle-orm/expressions';
 
 import { products } from '../tables/productsTable';
+import { suppliers } from '../tables/suppliersTable';
 
 export class Products {
   private db: BetterSQLite3Database;
@@ -28,6 +30,34 @@ OFFSET ?;`;
       })
       .limit(limit)
       .offset(offset)
+      .all();
+
+    return { sqlString, data };
+  }
+
+  async getProductById(id: number) {
+    const sqlString = `SELECT ProductID AS Id, ProductName AS Name, s.CompanyName AS Supplier, QuantityPerUnit AS 'Qt per unit', UnitPrice AS 'Unit price', 
+UnitsInStock AS 'Units in stock', UnitsOnOrder AS 'Units on order', ReorderLevel AS 'Reorder level', Discontinued 
+FROM Products AS p
+JOIN Suppliers AS s ON p.SupplierID = s.SupplierID 
+WHERE Id = ?;`;
+
+    const data = await this.db
+      .select(products)
+      .fields({
+        Id: products.productId,
+        ProductName: products.productName,
+        SupplierId: suppliers.supplierId,
+        Supplier: suppliers.companyName,
+        'Quantity per unit': products.quantityPerUnit,
+        'Unit price': products.unitPrice,
+        'Units in stock': products.unitsInStock,
+        'Units on order': products.unitsOnOrder,
+        'Reorder level': products.reorderLevel,
+        Discontinued: products.discontinued,
+      })
+      .leftJoin(suppliers, eq(products.supplierId, suppliers.supplierId))
+      .where(eq(products.productId, id))
       .all();
 
     return { sqlString, data };
