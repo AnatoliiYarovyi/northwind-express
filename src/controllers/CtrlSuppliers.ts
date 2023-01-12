94fb9932ddc9fc6ff750f2834c22b4ac059d23d2
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-01-12 14:44:21 +0200

    Added getSupplierById & updated getAllSuppliers

--- src/controllers/CtrlSuppliers.ts
+++ src/controllers/CtrlSuppliers.ts
@@ -1,12 +1,27 @@
 import { BetterSQLite3Database } from 'drizzle-orm-sqlite/better-sqlite3';
+import { Request } from 'express';
 
 import { Suppliers } from '../data/repositories/Suppliers';
 
 export class CtrlSuppliers {
-  async getAllSuppliers(req, res, next) {
+  async getAllSuppliers(req: Request, res, next) {
     const db: BetterSQLite3Database = req.body.connection;
+    const { limit, offset } = req.query;
+
     const suppliers = new Suppliers(db);
-    const data = await suppliers.getAllSuppliers();
+    const data = await suppliers.getAllSuppliers(+limit, +offset);
+
+    res.status(200).json({
+      status: 'success',
+      data,
+    });
+  }
+
+  async getSupplierById(req: Request, res, next) {
+    const db: BetterSQLite3Database = req.body.connection;
+    const { id } = req.params;
+    const suppliers = new Suppliers(db);
+    const data = await suppliers.getSupplierById(+id);
 
     res.status(200).json({
       status: 'success',
