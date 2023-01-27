efbe2657f5c0c2468037c083a53381ffd5231659
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-01-27 19:11:56 +0200

    Update metrics.ts

--- src/controllers/metrics.ts
+++ src/controllers/metrics.ts
@@ -2,12 +2,15 @@ function getTimeISO(): string {
   return new Date().toISOString();
 }
 
-function getTriggerDate(): number {
-  return new Date().getTime();
+function getTriggerDate(): [number, number] {
+  // return new Date().getTime();
+  return process.hrtime();
 }
 
-function getTimeInterval(triggerDate: number): number {
-  return getTriggerDate() - triggerDate;
+function getTimeInterval(triggerDate: [number, number]): number {
+  // return getTriggerDate() - triggerDate;
+  const timeInterval = process.hrtime(triggerDate);
+  return timeInterval[0] * 1000 + timeInterval[1] / 1000000;
 }
 
 export const metrics = { getTimeISO, getTriggerDate, getTimeInterval };
