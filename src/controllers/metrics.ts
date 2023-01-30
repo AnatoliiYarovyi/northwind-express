593dd459dc9fc5bb1d138a579fdea7111ecf91c1
Anatolii Yarovyi <69160081+AnatoliiYarovyi@users.noreply.github.com>
2023-01-30 15:35:30 +0200

    removed comments

--- src/controllers/metrics.ts
+++ src/controllers/metrics.ts
@@ -3,12 +3,10 @@ function getTimeISO(): string {
 }
 
 function getTriggerDate(): [number, number] {
-  // return new Date().getTime();
   return process.hrtime();
 }
 
 function getTimeInterval(triggerDate: [number, number]): number {
-  // return getTriggerDate() - triggerDate;
   const timeInterval = process.hrtime(triggerDate);
   return timeInterval[0] * 1000 + timeInterval[1] / 1000000;
 }
